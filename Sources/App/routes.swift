11a37fa45bb16249686909bcb9d1bc92e537f506
sans-connaissance <davemalicke@gmail.com>
2022-01-16 10:19:50 -0500

    Initial Commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,26 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    //http://127.0.0.1:8080
     app.get { req in
-        return "It works!"
+        return "ROOT"
     }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+ //http://127.0.0.1:8080/hello
+    app.get("hello") { req in
+        return "HELLO"
     }
+    
+    
+    
+    
+//    //http://127.0.0.1:8080/movies/genre/fiction
+//    app.get("movies", "genre", "fiction") { req in
+//        return "/movies/genre/fiction"
+//    }
+//
+//    //http://127.0.0.1:8080/movies/genre/comedy
+//    app.get("movies", "genre", "comedy") { req in
+//        return "/movies/genre/comedy"
+//    }
+    
 }
