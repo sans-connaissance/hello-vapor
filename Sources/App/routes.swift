e37838dce978e6ecc02d53fa6f1aa2febc834ea4
sans-connaissance <davemalicke@gmail.com>
2022-01-20 15:22:40 -0500

    CONTINUE route explorations

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -42,4 +42,53 @@ func routes(_ app: Application) throws {
     //        return "/movies/genre/comedy"
     //    }
     
+    // anything routes
+    app.get("foo", "*", "baz") { req in
+        return "FOO BAZZ"
+        
+    }
+    
+    // catchall routes
+    
+    app.get("foo", "**") { req in
+        return "FOO"
+    }
+    
+    //Query string
+    // search?keyword=toys&page=12
+    app.get("search") { req -> String in
+        guard let keyword = req.query["keyword"] as String?,
+              let page = req.query["page"] as Int? else {
+                  throw Abort(.badRequest)
+              }
+        
+        return "keyword = \(keyword) and page = \(page)"
+        
+    }
+    
+    //Route Groups
+    // /users/12
+    // /users
+    // POST /users
+    
+    let users = app.grouped("users")
+    
+    //users
+    users.get { req in
+        return "/users"
+    }
+    
+    //users/23
+    users.get(":userId") { req -> String in
+        guard let userId = req.parameters.get("userId") else {
+            throw Abort(.badRequest)
+        }
+        return "userId = \(userId)"
+    }
+    
+    //POST /users
+    users.post { req in
+        return "POST"
+    }
+    
 }
